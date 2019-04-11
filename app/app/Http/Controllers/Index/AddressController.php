134b3211d96715bd74d3ce43d7c9839423c4dad6
wangpeng <2337865364@qq.com>
2019-04-11 14:05:50 +0800

    1

--- app/app/Http/Controllers/Index/AddressController.php
+++ app/app/Http/Controllers/Index/AddressController.php
@@ -12,9 +12,15 @@ class AddressController extends Controller
 
     }
     public function getregion(Request $request){
+        $pid = $request->input('pid',1);
         if(empty($request)){
             $regin = DB::table('app_regio')->where('p_id',1)->get();
-            print_r($regin);
+            $data = json_decode($regin,1);
+            return $data;
+        }else{
+            $regin = DB::table('app_regio')->where('p_id',$pid)->get();
+            $data = json_decode($regin,1);
+            return $data;
         }
     }
 }
