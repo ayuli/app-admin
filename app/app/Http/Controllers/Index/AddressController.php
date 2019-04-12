85c5846a584b20f0b8de7da1e716ebec86fd8907
wangpeng <2337865364@qq.com>
2019-04-12 19:55:26 +0800

    1

--- app/app/Http/Controllers/Index/AddressController.php
+++ app/app/Http/Controllers/Index/AddressController.php
@@ -48,6 +48,34 @@ class AddressController extends Controller
             return 2;   //添加失败
         }
     }
+    public function upaddress(Request $request){
+        $consignee_name = $request->input('consignee_name');
+        $detailed_address = $request->input('detailed_address');
+        $province = $request->input('province');
+        $city = $request->input('city');
+        $district = $request->input('district');
+        $consignee_tel = $request->input('consignee_tel');
+        $is_address = $request->input('is_address');
+        if($is_address==1){
+            DB::table('app_address')->update(['is_address'=>0]);
+        }
+        $info = array(
+            'consignee_name'=>$consignee_name,
+            'detailed_address'=>$detailed_address,
+            'province'=>$province,
+            'city'=>$city,
+            'district'=>$district,
+            'consignee_tel'=>$consignee_tel,
+            'is_address'=>$is_address,
+
+        );
+        $res = DB::table('app_address')->update($info);
+        if($res){
+            return 1;   //修改成功
+        }else{
+            return 2;   //修改失败
+        }
+    }
         public function addressGet(Request $request){
             $name = $request->session()->get('user_name');
             if($name){
