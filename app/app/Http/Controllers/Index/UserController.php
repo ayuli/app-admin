ee8cde8c723c181e7a43b37842ca8cfe3a43e744
wangpeng <2337865364@qq.com>
2019-04-12 10:21:41 +0800

    1

--- app/app/Http/Controllers/Index/UserController.php
+++ app/app/Http/Controllers/Index/UserController.php
@@ -11,7 +11,7 @@ class UserController extends Controller
     public function register(Request $request){
         $name = $request->input('name');
         $pd = $request->input('pwd');
-        $pwd = sha1(md5(md5($pd)));
+        $pwd = md5(md5($pd));
         $time = time();
         $res = DB::table('app_user')->insert(['user_name'=>$name,'user_pwd'=>$pwd,'add_time'=>$time]);
         if($res){
