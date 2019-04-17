f800a5f0109dcccecbf19fcdfff0fc40b99b6fb4
q1328915484 <1328915484@qq.com>
2019-04-17 16:47:50 +0800

    w

--- app/app/Http/Controllers/Index/CartController.php
+++ app/app/Http/Controllers/Index/CartController.php
@@ -25,4 +25,36 @@ class CartController extends Controller
         $arr=json_encode($data);
         return $arr;
     }
+
+    //添加购物车
+    public function cartAdd(Request $request){
+        $data=$request->input();
+        $data['is_delete']=1;
+        $goods_num=$data['goods_num'];
+        $user_id=$data['user_id'];
+        $goods_id=$data['goods_id'];
+        $goods_attr=$data['goods_attr'];
+        $attr_price=DB::table('app_goods_attr')->whereIn('goods_attr_id',explode(',',$goods_attr))->pluck('goods_price');
+        $attr_price=array_sum($attr_price);
+        $goodsInfo=DB::table('app_goods')->where('goods_id',$goods_id)->first();
+        $goods_price=$goodsInfo->goods_price;
+        $goods_price=$goods_price+$attr_price;
+        $total_price=$goods_price*$goods_num;
+        $cartInsert=[
+            'goods_id'=>$goods_id,
+            'goods_num'=>$goods_num,
+            'goods_name'=>$goodsInfo->goods_name,
+            'goods_price'=>$goods_price,
+            'total_price'=>$total_price,
+            'user_id'=>$user_id,
+            'is_delete'=>1,
+            'add_time'=>time()
+        ];
+        $res=DB::table('app_cart')->insert($cartInsert);
+        if($res){
+            echo json_encode(['code'=>1,'msg'=>'加入购物车成功！']);
+        }else{
+            echo json_encode(['code'=>0,'msg'=>'请稍后重试！']);
+        }
+    }
 }
