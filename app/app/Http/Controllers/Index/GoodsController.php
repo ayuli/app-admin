d02173141d3edd5e907971a3c730b89b1906178a
q1328915484 <1328915484@qq.com>
2019-04-19 19:48:22 +0800

    w

--- app/app/Http/Controllers/Index/GoodsController.php
+++ app/app/Http/Controllers/Index/GoodsController.php
@@ -13,94 +13,125 @@ class GoodsController extends Controller
      */
     public function goods(Request $request)
     {
-        $type = $request->input('type','all');
-        $search = $request->input('search');
-        if($type=='all'){   //流加载
-            $page = $request->input('page');
-            $page_num = 6;
-            $start = ($page-1)*$page_num;
-            $arr = GoodsModel::offset($start)->limit($page_num)->get();
-            $count = count($arr);
-            if($count){
-                $data = ['code'  => 0, 'data'  =>$arr];
-                return json_encode($data,JSON_UNESCAPED_UNICODE);
-            }else{
-                $data = [ 'code'=>1102 , 'msg'=>'没有更多了!'];
-                return json_encode($data,JSON_UNESCAPED_UNICODE);
-            }
+//        $type = $request->input('type','all');
+//        $search = $request->input('search');
+//        if($type=='all'){   //流加载
+//            $page = $request->input('page');
+//            $page_num = 6;
+//            $start = ($page-1)*$page_num;
+//            $arr = GoodsModel::offset($start)->limit($page_num)->get();
+//            $count = count($arr);
+//            if($count){
+//                $data = ['code'  => 0, 'data'  =>$arr];
+//                return json_encode($data,JSON_UNESCAPED_UNICODE);
+//            }else{
+//                $data = [ 'code'=>1102 , 'msg'=>'没有更多了!'];
+//                return json_encode($data,JSON_UNESCAPED_UNICODE);
+//            }
+//
+//        }else if ($type=='hot'){
+//            $goods = GoodsModel::where(['is_hot'=>1])->limit(6)->get();
+//            $data = ['code'  => 0, 'data'  =>$goods];
+//            if($goods){
+//                return json_encode($data,JSON_UNESCAPED_UNICODE);
+//            }
+//        }else if($type=='new'){
+//            $page = $request->input('page');
+//            $page_num = 6;
+//
+//            $start = ($page-1)*$page_num;
+//            $arr = GoodsModel::where(['is_new'=>1])->offset($start)->limit($page_num)->get();
+//            $count = count($arr);
+//            if($count){
+//                $data = ['code'  => 0, 'data'  =>$arr];
+//                return json_encode($data,JSON_UNESCAPED_UNICODE);
+//            }else{
+//                $data = [ 'code' =>1102 , 'msg'=>'没有更多了!'];
+//                return json_encode($data,JSON_UNESCAPED_UNICODE);
+//            }
+//        }else if($type=='best'){
+//            $goods = GoodsModel::where(['is_best'=>1])->limit(6)->get();
+//            $data = ['code'  => 0, 'data'  =>$goods ];
+//            if($goods){
+//                return json_encode($data,JSON_UNESCAPED_UNICODE);
+//            }
+//        }else if($type=='pricex'){   //价格 降序
+//            $page = $request->input('page');
+//            $page_num = 6;
+//
+//            $start = ($page-1)*$page_num;
+//            $arr = GoodsModel::offset($start)->orderBy('goods_price','desc')->limit($page_num)->get();
+//
+//            $count = count($arr);
+//            if($count){
+//                $data = ['code'  => 0, 'data'  =>$arr];
+//                return json_encode($data,JSON_UNESCAPED_UNICODE);
+//            }else{
+//                $data = [ 'code'=>1102 , 'msg'=>'没有更多了!'];
+//                return json_encode($data,JSON_UNESCAPED_UNICODE);
+//            }
+//        }else if($type=='prices'){   //价格 升序
+//            $page = $request->input('page');
+//            $page_num = 6;
+//
+//            $start = ($page-1)*$page_num;
+//                $arr = GoodsModel::offset($start)->orderBy('goods_price','asc')->limit($page_num)->get();
+//
+//            $count = count($arr);
+//            if($count){
+//                $data = ['code'  => 0, 'data'  =>$arr];
+//                return json_encode($data,JSON_UNESCAPED_UNICODE);
+//            }else{
+//                $data = [ 'code'=>1102 , 'msg'=>'没有更多了!'];
+//                return json_encode($data,JSON_UNESCAPED_UNICODE);
+//            }
+//        }else if($type=='search'){
+//            $page = $request->input('page');
+//            $page_num = 6;
+//            $start = ($page - 1) * $page_num;
+//            $arr = GoodsModel::where('goods_name','like',"%$search%")->offset($start)->limit($page_num)->get();
+//            $count = count($arr);
+//            if ($count) {
+//                $data = ['code' => 0, 'data' => $arr];
+//                return json_encode($data, JSON_UNESCAPED_UNICODE);
+//            } else {
+//                $data = ['code' => 1102, 'msg' => '没有更多了!'];
+//                return json_encode($data, JSON_UNESCAPED_UNICODE);
+//            }
+//        }
 
-        }else if ($type=='hot'){
-            $goods = GoodsModel::where(['is_hot'=>1])->limit(6)->get();
-            $data = ['code'  => 0, 'data'  =>$goods];
-            if($goods){
-                return json_encode($data,JSON_UNESCAPED_UNICODE);
-            }
-        }else if($type=='new'){
-            $page = $request->input('page');
-            $page_num = 6;
+        $type=$request->input('type');
+        $search=$request->input('search');
 
-            $start = ($page-1)*$page_num;
-            $arr = GoodsModel::where(['is_new'=>1])->offset($start)->limit($page_num)->get();
-            $count = count($arr);
-            if($count){
-                $data = ['code'  => 0, 'data'  =>$arr];
-                return json_encode($data,JSON_UNESCAPED_UNICODE);
-            }else{
-                $data = [ 'code' =>1102 , 'msg'=>'没有更多了!'];
-                return json_encode($data,JSON_UNESCAPED_UNICODE);
-            }
-        }else if($type=='best'){
-            $goods = GoodsModel::where(['is_best'=>1])->limit(6)->get();
-            $data = ['code'  => 0, 'data'  =>$goods ];
-            if($goods){
-                return json_encode($data,JSON_UNESCAPED_UNICODE);
-            }
-        }else if($type=='pricex'){   //价格 降序
-            $page = $request->input('page');
-            $page_num = 6;
+        $where=[];
 
-            $start = ($page-1)*$page_num;
-            $arr = GoodsModel::offset($start)->orderBy('goods_price','desc')->limit($page_num)->get();
-
-            $count = count($arr);
-            if($count){
-                $data = ['code'  => 0, 'data'  =>$arr];
-                return json_encode($data,JSON_UNESCAPED_UNICODE);
-            }else{
-                $data = [ 'code'=>1102 , 'msg'=>'没有更多了!'];
-                return json_encode($data,JSON_UNESCAPED_UNICODE);
-            }
-        }else if($type=='prices'){   //价格 升序
-            $page = $request->input('page');
-            $page_num = 6;
-
-            $start = ($page-1)*$page_num;
-                $arr = GoodsModel::offset($start)->orderBy('goods_price','asc')->limit($page_num)->get();
-
-            $count = count($arr);
-            if($count){
-                $data = ['code'  => 0, 'data'  =>$arr];
-                return json_encode($data,JSON_UNESCAPED_UNICODE);
-            }else{
-                $data = [ 'code'=>1102 , 'msg'=>'没有更多了!'];
-                return json_encode($data,JSON_UNESCAPED_UNICODE);
-            }
-        }else if($type=='search'){
-            $page = $request->input('page');
-            $page_num = 6;
-            $start = ($page - 1) * $page_num;
-            $arr = GoodsModel::where('goods_name','like',"%$search%")->offset($start)->limit($page_num)->get();
-            $count = count($arr);
-            if ($count) {
-                $data = ['code' => 0, 'data' => $arr];
-                return json_encode($data, JSON_UNESCAPED_UNICODE);
-            } else {
-                $data = ['code' => 1102, 'msg' => '没有更多了!'];
-                return json_encode($data, JSON_UNESCAPED_UNICODE);
-            }
+        if($type=="pricex"){
+            $order=[
+                'goods_price'=>"asc"
+            ];
+        }else if($type=="prices"){
+            $order=[
+                'goods_price'=>"desc"
+            ];
+        }else{
+            $order=[
+                'add_time'=>'desc'
+            ];
+            $where=[
+                "$type"=>1
+            ];
         }
 
+        $arr = GoodsModel::where('goods_name','like',"%$search%")->where($where)->orderBy($order)->paginate(6);
 
+        $count = count($arr);
+        if ($count) {
+            $data = ['code' => 0, 'data' => $arr];
+            return json_encode($data, JSON_UNESCAPED_UNICODE);
+        } else {
+            $data = ['code' => 1102, 'msg' => '没有更多了!'];
+            return json_encode($data, JSON_UNESCAPED_UNICODE);
+        }
     }
 
 }
