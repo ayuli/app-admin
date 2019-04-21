e1c5a4acb119b3fb4d10b4fed5c35276936e6a58
q1328915484 <1328915484@qq.com>
2019-04-21 22:10:09 +0800

    w

--- app/app/Http/Controllers/Index/AddressController.php
+++ app/app/Http/Controllers/Index/AddressController.php
@@ -95,7 +95,7 @@ class AddressController extends Controller
                         $city=DB::table('app_region')->where('region_id',$v->city)->value('region_name');
                         $district=DB::table('app_region')->where('region_id',$v->district)->value('region_name');
                         $arr[$k]['addressInfo']=$province.$city.$district.$v->detailed_address;
-                        $arr[$k]['address_id']=$v->address_id;
+                        $arr[$k]['address_id']=$v->id;
                         $arr[$k]['userInfo']=$v->consignee_name." ".$v->consignee_tel;
                     }
                     echo json_encode(['code'=>1,'data'=>$arr]);
