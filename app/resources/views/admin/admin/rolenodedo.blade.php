b9bc5146591b65c4e23e597f8c8431a4b7aa1c95
zhaoyifeng <790259148@qq.com>
2019-04-23 19:10:24 +0800

    grwb

---
+++ app/resources/views/admin/admin/rolenodedo.blade.php
@@ -0,0 +1,24 @@
+
+@foreach($info as $v)
+     <input type="checkbox" name="like[write]"   node_id="{{$v->node_id}}"  title="{{$v->node_name}}">
+        <div class="layui-unselect layui-form-checkbox " lay-skin>
+            <span class="{{$names}}">{{$v->node_name}}</span>
+            <i class="layui-icon {{$names}}" ></i>
+        </div>
+ @endforeach
+
+
+<script>
+    $(function(){
+        var wwww="{{$names}}";
+        $('.'+wwww+'').click(function(){
+//            alert(123)
+            if($(this).parent().hasClass("layui-form-checked")){
+                $(this).parent().removeClass('layui-form-checked');
+            }else{
+                $(this).parent().addClass('layui-form-checked');
+            }
+
+        })
+    })
+</script>
